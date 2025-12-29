4e9b1c24d7197e06ebd26822c22183d7b30070a1
kushw <kushwahasahitya909@gmail.com>
2025-12-29 22:30:08 +0530

    Add support for state filtering in college search and update UI

--- JavaScript/Advanced JS/app6.js
+++ JavaScript/Advanced JS/app6.js
@@ -1,9 +1,10 @@
 let url='http://universities.hipolabs.com/search?country=';
 
 
-async function getCollege(country) {
+async function getCollege(country,state) {
     try{
-        let res=await axios.get(url+country);
+        let res=await axios.get(url+country+'&state-province='+state);
+        console.log(res.data);
         return res.data;
     }catch(error){
         console.log("Error:",error);
@@ -14,18 +15,20 @@ async function getCollege(country) {
 // getCollege();
 
 let button=document.querySelector('button');
-let input=document.querySelector('input');
-// let body=document.querySelector('body');
+let input=document.querySelector('#i1');
+let input2=document.querySelector('#i2');
 let ul=document.querySelector('ul');
 
 button.addEventListener('click',async()=>{
     let country=input.value;
-    let colArr=await getCollege(country);
+    let state=input2.value;
+    let colArr=await getCollege(country,state);
     // console.log(colArr);
     show(colArr);
 })
 
 function show(colArr){
+    ul.innerHTML='';
     for(col of colArr){
         console.log(col.name);
         let li=document.createElement('li');
