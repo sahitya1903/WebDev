c6ee6483d615dc98d0ed93e9ed3033ba80b202ea
kushw <kushwahasahitya909@gmail.com>
2025-12-29 12:39:56 +0530

    Refactor color change promise usage and add async/await demo functions

--- Advanced JS/app3.js
+++ Advanced JS/app3.js
@@ -9,8 +9,7 @@ function changeColor(color,delay){
     })
 }
 
-let request=changeColor('red',1000);
-console.log(request);
+/*
 
 changeColor('red',1000)
     .then(()=>{
@@ -28,3 +27,54 @@ changeColor('red',1000)
     .then(()=>{
         console.log('color changed to blue');
     })
+
+*/
+
+async function greet() {
+    // abc.abc();  //promise rejected as abc not defined
+    // throw '404 page not found';      //return promise rejected with error
+    return 'hello';     //returns promise result: hello
+}
+
+
+greet()
+    .then((result)=>{
+        console.log('promise resolved with result: ',result);
+    })
+    .catch((err)=>{
+        console.log('promise rejected with error:',err);
+    });
+
+let func= async ()=>{           //async arrow function
+    return 5;
+}
+
+
+function getNum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let num=Math.floor(Math.random()*10)+1;
+            if(num>5){
+                reject('promise rejected');
+            }
+            console.log(num);
+            resolve('got random number');
+        },1000);
+    });
+}
+
+async function demo() {
+    try{
+        await getNum();
+        await getNum();
+        await getNum();
+    }catch(err){
+        console.log('error caught: ',err);
+    }
+    // await changeColor('red',1000);
+    // await changeColor('green',1000);
+    // await changeColor('blue',1000);
+    // changeColor('pink',1000);
+}
+
+demo();
