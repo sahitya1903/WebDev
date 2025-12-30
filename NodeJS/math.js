ae9430554b4983e9f0f1a64c800355babbf0b5aa
kushw <kushwahasahitya909@gmail.com>
2025-12-30 17:01:46 +0530

    Refactor math.js to export an object with sum, mul, PI, and g properties

--- NodeJS/math.js
+++ NodeJS/math.js
@@ -3,4 +3,18 @@ const PI=3.14;
 const sum=(a,b)=>a+b;
 const mul=(a,b)=>a*b;
 
-module.exports=123;
+const obj={
+    sum:sum,
+    mul:mul,
+    PI:PI,
+    g:g
+}
+
+module.exports=obj;
+
+exports.k=1;  //Not works if module.exports is overwritten by new object
+
+// module.exports={
+//     name:'sahitya',
+//     marks:100
+// };
