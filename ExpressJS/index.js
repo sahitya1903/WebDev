05dccc58d6240f0b34bbfb08b809c85e1a4bdf97
kushw <kushwahasahitya909@gmail.com>
2025-12-31 19:39:02 +0530

    Refactor ExpressJS app: comment out debug and legacy code, add route handlers

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 
-console.dir(app);
+// console.dir(app);
 
 let port=8080;  //or 3000
 
@@ -9,9 +9,35 @@ app.listen(port, ()=>{
     console.log(`app listening on port${port}`);
 })
 
-app.use((req,res)=>{
-    console.log("Request received");
-    let code="<h1>Fruits</h1> <ul><li>apple</li><li>mango</li></ul>";
+// app.use((req,res)=>{
+//     console.log("Request received");
+//     let code="<h1>Fruits</h1> <ul><li>apple</li><li>mango</li></ul>";
+//     res.send(code);
+// })
 
-    res.send(code);
+app.get("/",(req,res)=>{
+    res.send("You contacted root path by get request");
+})
+
+app.get("/apple",(req,res)=>{
+    res.send("You contacted apple path by get request");
+})
+
+app.get("/orange",(req,res)=>{
+    res.send("You contacted orange path by get request");
+})
+
+app.get("/:username",(req,res)=>{
+    console.log(req.params);
+    res.send("Hello,I'm root");
+})
+
+app.post('/',(req,res)=>{
+    res.send("You sent a post request to root path");
+})
+
+
+//All other kind of requests (Always put in last)
+app.use((req,res)=>{
+    res.send("This path does not exist");
 })
