22a1b7893a7a15498d31370d1760ea1c62a73377
kushw <kushwahasahitya909@gmail.com>
2026-01-01 18:08:35 +0530

    Set up EJS view engine and add home page route with template

--- EJS/index.js
+++ EJS/index.js
@@ -3,8 +3,17 @@ const app=express();
 
 const port=8080;
 
+const path=require('path');
+
+app.set('views',path.join(__dirname,'/views'));     //set ejs path
+
 app.set('view engine','ejs');
 
 app.listen(port,()=>{
     console.log(`listening on port: ${port}`);
+})
+
+app.get("/",(req,res)=>{
+    // res.send("This is home");
+    res.render("home");
 })
