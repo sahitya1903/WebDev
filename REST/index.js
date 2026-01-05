018f7e79ae5311fb75a968386b2232bde262e45c
kushw <kushwahasahitya909@gmail.com>
2026-01-05 22:06:13 +0530

    Add method-override dependency and integrate method override middleware

--- REST/index.js
+++ REST/index.js
@@ -4,9 +4,10 @@ const port=8080;
 const path=require('path');
 
 const {v4:uuidv4}=require('uuid');      //UUID package
-
+const methodOverride=require('method-override');
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
