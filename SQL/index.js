012d184379c9c5a176c95455051c4251ae7f4b71
kushw <kushwahasahitya909@gmail.com>
2026-01-08 19:21:24 +0530

    Refactor user data generation and bulk insertion in SQL/index.js

--- SQL/index.js
+++ SQL/index.js
@@ -1,6 +1,25 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
+// let getRandomUser=()=>{
+  // return {                         //object key value pair fake data
+  //   userId: faker.string.uuid(),
+  //   username: faker.internet.username(),
+  //   email: faker.internet.email(),
+  //   password: faker.internet.password()
+  // };
+// };
+
+let getRandomUser=()=>{
+  return [                            //array of fake data values
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password()
+  ];
+};
+
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -8,22 +27,33 @@ const connection = mysql.createConnection({
   password:'root'
 });
 
+//Single Row insertion
+// let q="INSERT INTO user (id,username,email,password) VALUES (?,?,?,?)"; 
+// let user=['123','123_new','abc@gmail.com','abc'];
+
+//Multiple rows insertion
+// let users=[
+//   ['123b','123b_new','abcd@gmail.com','abcd'],
+//   ['123c','123c_new','bc@gmail.com','bc']
+// ];
+
+let q="INSERT INTO user (id,username,email,password) VALUES ? ";
+
+//Inserting new fake data
+let data=[];
+for(let i=0;i<100;i++){
+  data.push(getRandomUser());   //100 users fake data
+}
+
 try{
-    connection.query("SELECT * FROM TEACHER",(err,result)=>{
+    connection.query(q,[ data ] ,(err,result)=>{
         if(err) throw err;
         console.log(result);
+        console.log(result.length);
     });
-}catch(err){
-    console.log(result);
+} catch(err){
+    console.log(err);
 }
+connection.end();
 
-let getRandomUser=()=>{
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password()
-  };
-}
 
-// console.log(getRandomUser());
