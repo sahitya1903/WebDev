9d59574452fa0feeae6fd9c03251663ddda41a4c
kushw <kushwahasahitya909@gmail.com>
2026-03-24 02:18:13 +0530

    Added code for using connect-flash package

--- Routing/server2.js
+++ Routing/server2.js
@@ -3,12 +3,33 @@ const app=express();
 const users=require('./routes/user');
 const posts=require('./routes/post');
 const session=require('express-session');
+const flash=require('connect-flash');
+const path = require('path');
 
-app.use(session({
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+const sessionOptions= {
     secret:'mysupersecretstring',
     resave:false,
     saveUninitialized:true
-}));
+}
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get('/register',(req,res)=>{
+    let {name='anonymous'}=req.query;
+    req.session.name=name;
+    req.flash('success','user registered successfully');
+    // res.send(name);
+    res.redirect('/hello');
+})
+
+app.get('/hello',(req,res)=>{
+    // console.log(req.flash('success'));
+    res.render('page.ejs',{ name:req.session.name, msg: req.flash('success') });
+})
 
 app.get('/reqcount',(req,res)=>{
     if(req.session.count){
