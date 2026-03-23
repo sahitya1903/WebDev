202dc0491938c6a0cea56df97afaf3315cb04dc9
kushw <kushwahasahitya909@gmail.com>
2026-03-23 22:55:54 +0530

    Added code for cookie parsing and signed cookies

--- Routing/server.js
+++ Routing/server.js
@@ -2,9 +2,36 @@ const express=require('express');
 const app=express();
 const users=require('./routes/user');
 const posts=require('./routes/post')
+const cookieParser=require('cookie-parser');
+
+// app.use(cookieParser());
+app.use(cookieParser('secretcode'));
+
+app.get('/getsignedcookies',(req,res)=>{
+    res.cookie('made-in','india',{signed:true});
+    res.send('signed cookie sent');
+})
+
+app.get('/verify',(req,res)=>{
+    console.dir(req.cookies);
+    console.dir(req.signedCookies);
+    res.send('verified'); 
+})
+
+app.get('/getcookies',(req,res)=>{
+    res.cookie('greet','hello');
+    res.cookie('apple','made in india')
+    res.send('sent you some cookies');
+})
+
+app.get('/greet',(req,res)=>{
+    let {name='anonymous'}=req.cookies;
+    res.send(`Hello,I am ${name}`)
+})
 
 //ROOT
 app.get('/',(req,res)=>{
+    console.dir(req.cookies);
     res.send("Hello I'm root");
 });
 
