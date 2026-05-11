09d1dbccd768649e88ae640c334300ab6fad5ca2
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-11 20:47:28 +0530

    Rendering arrays

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,10 +1,14 @@
 import './Product.css';
-function Product({title,price=1}){
-    // console.log(props.title);
-    return(
+function Product({ title, price = 1, features = [] }) {
+    return (
         <div className="Product">
-        <h3>{title}</h3>
-        <h5>Price: {price*.8}</h5>   
+            <h3>{title}</h3>
+            <h5>Price: {price * .8}</h5>
+            <ul>{features.map((feature) => (
+                <li>{feature}</li>
+            ))}
+            </ul>
+
         </div>
     );
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,10 +1,12 @@
 import Product from "./Product";
 function ProductTab(){
+    let options=['hi-tech','durable','fast'];
+    // let options2=[<li>'hi-tech'</li>,<li>'durable'</li>,<li>'fast'</li>];
     return(
         <>
-            <Product title='phone' price={30000}/>
+            <Product title='phone' price={30000} features={options}/>
             <Product title='laptop' price={80000}/>
-            <Product title='pen'/>
+            <Product title='pencil'/>
         </>
     )
 }
