dcc69963e6e3c507eb0659b79e72f37a7b3118ca
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-11 11:36:59 +0530

    Adding Title and Description component

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
+import Title from './Title';
+
+
+function Description(){
+  return (
+    <h3>I am Description</h3>
+  );
+}
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>This is my app component</h1>
+      <Title/>
+      <Description/>
+    </>
+  );
 }
 
 export default App;
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,11 @@
+function Title(){
+  let name='Sahitya'
+  return (
+    <>
+    <h1>Hi, {name.toUpperCase()}</h1>
+    <p>I am Title</p>       
+    </> 
+  );
+}
+
+export default Title; 
