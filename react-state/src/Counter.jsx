aa89f9e7c38eec7cf9ced917533c310667e1d359
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-14 00:30:54 +0530

    Adding example for re rendering of components and callback in updater function

--- react-state/src/Counter.jsx
+++ react-state/src/Counter.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 
+function init(){
+    console.log('init executed')
+    return Math.random();
+}
+
 export default function Counter(){
-    let [count,setCount]=useState(0);
+    let [count,setCount]=useState(init);    //initialisation
+    console.log('component rendered');
+    // console.log(`${count}`)
 
     let incCount=()=>{
-        setCount(count+1);
-        console.log(count+1);
-     }
+        // setCount(count+1);      //async behaviour
+        // setCount(count+1);
+        // setCount(count+1);
+        // setCount(count+1);
+
+        setCount((curr)=>{      //sync behaviour
+            return curr+1;
+        })
+        // setCount((curr)=>{
+        //     return curr+1;
+        // })
+        // console.log(count);
+    }
 
     return (
         <div>
