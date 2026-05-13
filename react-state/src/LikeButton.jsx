b057a70b6b6b744c9946cfed878eee39d0509232
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-13 23:35:37 +0530

    Added like button toogle and click counter

--- react-state/src/LikeButton.jsx
+++ react-state/src/LikeButton.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function LikeButton(){
+    let [isLiked,setIsLiked]=useState(false);
+    let [click,setClick]=useState(0);
+
+    let toggleLike=()=>{
+        setIsLiked(!isLiked);
+        setClick(click+1);
+        console.log('clicked')
+    }
+
+
+    let likeStyle={color:'red'};
+
     return(
-        <div>
-            <i class="fa-regular fa-heart"></i>
-        </div>
+        <p onClick={toggleLike}>
+            {/* {isLiked.toString()} */}
+            No. of Clicks: {click}
+            {
+                isLiked ? (<i className="fa-solid fa-heart" style={likeStyle}></i>):(<i className="fa-regular fa-heart"></i>)
+            }
+        </p>
     );
 }
