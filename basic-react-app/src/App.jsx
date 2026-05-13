876efb1b33b489867e96006acecf7f498f9bca1e
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-13 16:42:29 +0530

    Event handlers in React

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css';
 import ProductTab from './ProductTab';
 import Hello from './Hello';
+import Form from './Form';
 
+function printHello(event){
+  console.log('Hello');
+  console.log(event);
+}
+
+function printBye(){
+  console.log('Bye');
+}
+
+function handleDblClick(){
+  console.log('Double click occured');
+}
 
 function App() {
   return (
     <>
       <Hello username="Sahitya" textColor="green"/>
       <ProductTab/>
+      <button onClick={printHello}>Click me!</button>
+      <p onMouseOver={printBye}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, rem. Temporibus consequatur dolore incidunt ad, placeat fugit omnis veniam repellat maiores tempore laborum! Officia ex minima libero voluptatem non pariatur?</p>
+      <button onDoubleClick={handleDblClick}>Double Click me</button>
+      <Form/>
     </>
   );
 }
