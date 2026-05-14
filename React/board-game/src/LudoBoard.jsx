fc350279afcf2086c5d1199818b26510bd2a4e25
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-15 00:51:34 +0530

    Used array ad state variable in board-game

--- React/board-game/src/LudoBoard.jsx
+++ React/board-game/src/LudoBoard.jsx
@@ -2,16 +2,32 @@ import { useState } from "react"
 
 export default function LudoBoard() {
     let [moves, setMoves] = useState({ blue: 0, red: 0, yellow: 0, green: 0 });
+    let [arr, setArr]= useState(['no moves']); 
+
     let inc = (col) => {
+    //spread operator used for making address copy to change state variable to trigger rerendering of component 
         setMoves(
             (prevMoves) => ({...prevMoves, [col]:prevMoves[col]+1,}
         ));
-        console.log(`${col} moves= ${moves[col]}`)
+        console.log(`${col} moves= ${moves[col]}`);
+
+        //change in original array, so no rerendering
+        // arr.push(`${col} moves `);
+        // setArr(arr);
+
+        //spread array and then set
+        // setArr([...arr,` ${col}`]) //this is also correct way
+
+        setArr((prevMoves)=>{
+            return [...prevMoves,` ${col}`]
+        })
+        console.log(arr);
     }
 
     return (
         <div>
             <p>Game Begins</p>
+            <p>{arr}</p>
             <div className="board">
                 <p>Blue Moves: {moves.blue}</p>
                 <button style={{ backgroundColor: 'blue' }} onClick={() => inc('blue')}>+1</button>
