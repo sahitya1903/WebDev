80b35496a5f568b669d6cd9418564c771f9da698
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-15 15:56:36 +0530

    Design a basic todo list using react

---
+++ React/todo-list/src/TodoList.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+
+export default function TodoList(){
+    let [todos,setTodos]=useState(['sample Task']);
+    let [newTodo,setNewTodo]=useState("");
+
+    let addNewTask=()=>{
+        setTodos([...todos,newTodo]);
+        setNewTodo("");
+    }
+
+    let updateTodoValue=(event)=>{
+        setNewTodo(event.target.value)
+    }
+
+    return(
+        <>
+            <input type="text" placeholder='Add a task' value={newTodo} onChange={updateTodoValue}/>
+            <button onClick={addNewTask}>Add Task</button>
+            <br /><br /><br /><br />
+            <hr />
+            <h4>Todo List</h4>
+            <ul>
+                {
+                    todos.map((todo)=>
+                    <li>{todo}</li>
+                )}
+            </ul>
+        </>
+    )
+}
