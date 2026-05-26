f160cae7ed02e2db8f9e8b4bf87f28ebfbe5d904
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-27 00:56:51 +0530

    Restructured the code and changed styling of lottery game

--- React/lottery-game/src/Lottery.jsx
+++ React/lottery-game/src/Lottery.jsx
@@ -1,38 +1,25 @@
 import { useState } from "react";
+import { generateTicket,sumArr } from "./helper";
+import './Lottery.css'
 
 export default function Lottery() {
-    let [arr, setArr] = useState(['']);
+    let [ticket, setTicket] = useState(generateTicket(3));
     let [win, setWin] = useState('');
 
-    function sumArr(array) {
-        let sum = 0;
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum;
-    }
-
-    let ticket = () => {
-        let newArr = [
-            Math.floor(Math.random() * 10),
-            Math.floor(Math.random() * 10),
-            Math.floor(Math.random() * 10)
-        ]
-        let digitsum = sumArr(newArr);
-        setArr(() => {
-            return newArr
-        });
+    let buyTicket = () => {
+        let newTicket = generateTicket(3);
+        let digitsum = sumArr(newTicket);
+        setTicket(()=>newTicket);
         setWin(() => {
             return digitsum === 15 ? 'Congratulations, You Win' : 'Better Luck Next Time'
         });
     };
 
-
     return (
-        <div>
+        <div className="ticket">
             <h3>Lottery Game</h3>
-            <p>Lottery Ticket={arr}</p>
-            <button onClick={ticket}>Get new ticket</button>
+            <p>Lottery Ticket={ticket}</p>
+            <button onClick={buyTicket}>Get new ticket</button>
             <h3>{win}</h3>
         </div>
     );
