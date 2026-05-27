f7e086d66f085209c8748928fac8d4e2cf51f251
sahitya1903 <kushwahasahitya909@gmail.com>
2026-05-27 20:29:05 +0530

    Redesigned code structure using state design pattern

--- React/lottery-game/src/App.jsx
+++ React/lottery-game/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Lottery from './Lottery'
+import Ticket from './Ticket'
 
 function App() {
 
   return (
     <>
-      <Lottery/>
+      {/* <Lottery/> */}
+      {/* <TicketNum num={5}/>
+      <TicketNum num={4}/>
+      <TicketNum num={3}/> */}
+      <Ticket ticket={[1,2,3]}/>
     </>
   )
 }
